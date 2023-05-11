7ce070ac2cfbc0713fd2ce7ce33046941da4bf1e
Achref LAABIDI <abidi.achref95@gmail.com>
2023-05-11 17:45:45 +0100

    nearly

--- index.js
+++ index.js
@@ -1,33 +1,47 @@
-//main menu
+//main menu elements
 const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
 
-//main meny h1
-const h1 = document.createElement('h1')
-h1.innerHTML = "Psycho Game"
-canvas.parentNode.insertBefore(h1, canvas.nextSibling)
-
-//main menu img
 const img = new Image()
 img.onload = function() {
     ctx.drawImage(img, 0, 0)
-};
+    ctx.font = "18px Courier New"
+    ctx.fillStyle = 'white'
+    ctx.fillText("Psycho Game", 95, 40)
+}
 img.src = 'resources/images/main-menu.jpg'
 
-// Create START button element
+// Create START and MUSIC buttons
 const button = document.createElement('button')
 button.innerText = 'Start Game'
-// Set button class
 button.className = 'glow-on-hover'
-// Append start button to canvas
 canvas.parentNode.appendChild(button)
 
-
 const musicButton = document.createElement('button')
-
 musicButton.className = 'music-btn'
 canvas.parentNode.appendChild(musicButton)
+//MUSIC!!!!
+const audio = new Audio('resources/sounds/password-infinity.mp3')
+const musicOn = document.querySelector('.music-btn')
+musicOn.addEventListener('click', function() {
+  if (audio.paused) {
+    audio.play()
+  } else {
+    audio.pause()
+  }
+})
 
+//Start the game by clearing everything
+button.addEventListener('click', function() {
+    // Clear the canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    button.remove()
+    musicButton.remove()
+    
+    //audio.pause()
 
-
-
+    //load the game
+    const script = document.createElement('script')
+    script.src = 'game.js'
+    document.body.appendChild(script)
+})
---
+++ instructions.js
@@ -0,0 +1,5 @@
+const h3 = document.createElement('h2')
+h3.style.textAlign = "center";
+h3.innerHTML = "The rules are simple:<br><span style='font-size:24px'>catch as many stressed emojis as you can.</span><br><span style='font-size:20px'>Good luck</span>"
+h3.style.padding = "30px";
+canvas.parentNode.insertBefore(h3, canvas.nextSibling)
