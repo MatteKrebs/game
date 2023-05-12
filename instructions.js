514c99ccfc11833186bea992e12a3fe4b9136eff
Achref LAABIDI <abidi.achref95@gmail.com>
2023-05-12 21:09:11 +0100

    final

--- instructions.js
+++ instructions.js
@@ -6,7 +6,7 @@ backgroundImg1.src = 'resources/images/space.png'
 
 const h3 = document.createElement('h2')
 h3.style.textAlign = "center";
-h3.innerHTML = "The rules are simple:<br><span style='font-size:24px'>catch as many stressed emojis as you can.</span><br><span style='font-size:20px'>Good luck</span>"
+h3.innerHTML = "The rules are simple:<br><span style='font-size:24px'>catch as many stressed emojis as you can and try to exceed stress LVL 100.</span><br><span style='font-size:20px'>Good luck</span>"
 h3.style.padding = "30px";
 canvas.parentNode.insertBefore(h3, canvas.nextSibling)
 
