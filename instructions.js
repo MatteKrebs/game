6313dddff5e89f67ea7a097e4698894255ba6b31
Achref LAABIDI <abidi.achref95@gmail.com>
2023-05-12 21:27:52 +0100

    final version

--- instructions.js
+++ instructions.js
@@ -6,7 +6,7 @@ backgroundImg1.src = 'resources/images/space.png'
 
 const h3 = document.createElement('h2')
 h3.style.textAlign = "center";
-h3.innerHTML = "Use left and right arrow to mopve the player<br><span style='font-size:24px'>Catch as many stressed emojis as you can and avoid the YOGA ones.</span><br><span style='font-size:30px; color:red;'>Good luck</span>"
+h3.innerHTML = "Work is stressful.<br><span style='font-size:24px'>Guide you're overworked player right and left using the arrow keys (<-- left, right -->).</span><br><span style='font-size:24px'>Gather the RED stressed emojis to build your work tolerance, while avoiding the WHITE corporate imposed yoga classes!</span>"
 h3.style.padding = "30px";
 canvas.parentNode.insertBefore(h3, canvas.nextSibling)
 
@@ -17,3 +17,4 @@ canvas.parentNode.appendChild(button3)
 button3.addEventListener('click', function() {
   window.location.reload()
 })
+
